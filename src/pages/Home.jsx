af831d76fe5d6faad26cf922ae2f93e8d3fa98f7
James H <jimhitchman@hotmail.com>
2024-05-31 16:36:11 +0100

    displayed shows and actors data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from './../components/SearchForm';
+import ShowGrid from '../components/shows/ShowGrid';
+import ActorsGrid from '../components/actors/ActorsGrid';
 
 const Home = () => {
   const [apiData, setApiData] = useState(null);
@@ -27,13 +29,15 @@ const Home = () => {
     if (apiDataError) {
       return <div>An Error occured, please try again</div>;
     }
-    if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+
+    if(apiData?.length === 0){
+      return <div>No results</div>
     }
+
+    if (apiData) {
+      return apiData[0].show ? <ShowGrid shows={apiData} /> : <ActorsGrid actors={apiData}/>
+    }
+    return null;
   };
 
   return (
