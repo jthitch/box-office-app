e268a8474fd2753f485bd689acfa28673197e272
James H <jimhitchman@hotmail.com>
2024-06-12 17:27:12 +0100

    used useQuert on homepage to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,21 @@ import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from './../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query'
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setapiDataError] = useState(null);
+
+  const [filter, setFilter] = useState(null)
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () => filter.searchOption === 'shows' ? searchForShows(filter.q) : searchForPeople(filter.q),
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  })
 
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setapiDataError(null);
-
-      let result;
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setapiDataError(error);
-    }
+    setFilter({q, searchOption})
   };
 
   const renderApiData = () => {
@@ -51,5 +47,6 @@ const Home = () => {
     </div>
   );
 };
+
 
 export default Home;
