d97d355d1563f687a3bc6f7f5d3b5f8e756bb97b
James H <jimhitchman@hotmail.com>
2024-06-14 12:30:19 +0100

    added cast, seasons, details on show show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -2,12 +2,19 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 import { useQuery } from '@tanstack/react-query'
+import ShowMainData from '../components/shows/ShowMainData';
+import Details from '../components/shows/Details';
+import Seasons from '../components/shows/Seasons';
+import Cast from '../components/shows/Cast';
+
+
 
 const Show = () => {
   const { showId } = useParams();
   const {data: showData, error: showError } = useQuery({
     queryKey:['show',showId],
-     queryFn:() => getShowById(showId)
+     queryFn:() => getShowById(showId),
+     refetchOnWindowFocus: false,
     });
 
   
@@ -17,7 +24,25 @@ const Show = () => {
   }
 
   if (showData) {
-    return <div>Got show data: {showData.name}</div>;
+    return <div>
+      <ShowMainData image={showData.image} name={showData.name} rating={showData.rating} summary={showData.summary} genres={showData.genres}/>
+
+      <div>
+        <h2>Details</h2>
+        <Details status={showData.status} premiered={showData.premiered} network={showData.network} />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={showData._embedded.seasons}/>
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={showData._embedded.cast} />
+      </div>
+
+    </div>;
   }
 
   return <div>Data is loading... {showId}</div>;
