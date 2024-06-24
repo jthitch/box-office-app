44be6bbe10ccc4f89bc3cf9557a829e03872bd71
James H <jimhitchman@hotmail.com>
2024-06-24 16:15:53 +0100

    added github homepage, changed browserrouter to hashrouter for client side routing. changed anchor tag to Link to use prefix for linking to pages

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { SearchCard, SearchImgWrapper } from "../common/SearchCard";
 import { StarIcon } from "../common/StarIcon";
 import { useRef } from "react";
+import { Link } from 'react-router-dom'
 
 const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred}) => {
   const summaryStripped = summary
@@ -32,7 +33,7 @@ const handleStarClick = () => {
 
       <p>{summaryStripped}</p>
       <ActionSection>
-        <a href={`/show/${id}`} target="_blank" rel="noreferrer">Read more</a>
+        <Link to={`/show/${id}`} target="_blank" rel="noreferrer">Read more</Link>
         <StarBtn ref={starBtnRef} type="button" onClick= {handleStarClick}>
           <StarIcon active={isStarred}/>
           {/* {isStarred ? "Unstar me" : "Star Me"} */}
